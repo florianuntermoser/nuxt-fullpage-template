9146d0596cc5df6b4db97ebd16272981359cc3ac
Florian Untermoser <florian@untermoser.com>
2023-08-26 22:11:56 +0200

    add tailwind + scrollsnapping

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="snap-y snap-mandatory h-screen overflow-y-scroll">
+    <section class="snap-center snap-always h-full flex items-center justify-center bg-black/10">
+      <p>Section 1</p>
+    </section>
+    <section class="snap-center snap-always h-full flex items-center justify-center">
+      <p>Section 2</p>
+    </section>
+    <section class="snap-center snap-always h-full flex items-center justify-center bg-black/10">
+      <p>Section 3</p>
+    </section>
+  </div>
+</template>
